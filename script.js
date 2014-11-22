a9d3fda86de795023077889f677add5913b9ab47
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-22 09:16:04 -0500

    Added script.js and styles.css, included game logic

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	var xTurn = true;
+	var xScore = 0;
+	var oScore = 0;
+
+function render () {
+	$("#xScore").text("x Score: " + xScore);
+	$("#oScore").text("o Score: " + oScore);
+}
+	render();
+
+	$("td").on("click", function () {
+		var self = $(this);
+		if (xTurn) {
+			self.text("X");
+			self.addClass("x");
+		}
+		else {
+			$(this).text("O");
+			self.addClass("o");
+		}
+		xTurn = !xTurn;
+		$(this).off("click");
+		gameEval();
+	});
+function gameEval () {
+	if  (
+		$("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') ||
+		$("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') ||
+		$("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') ||
+		$("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') ||
+		$("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') ||
+		$("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') ||
+		$("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') ||
+		$("#three").hasClass('x') && $("#two").hasClass('x') && $("#seven").hasClass('x')
+		) {
+		xScore += 1;
+		render();
+		alert("Player X has won the game!");
+	}
+	else if (
+		$("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') ||
+		$("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') ||
+		$("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') ||
+		$("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') ||
+		$("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') ||
+		$("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') ||
+		$("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') ||
+		$("#three").hasClass('o') && $("#two").hasClass('o') && $("#seven").hasClass('o')
+		) {
+		yScore += 1;
+		render();
+		alert("Player O has won the game!");
+		}
+	}
+
+});
+
+
+
+
