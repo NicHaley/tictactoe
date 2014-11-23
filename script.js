8584c5e9a98673544a36eb3fe8655c14e4ef441e
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-23 11:52:16 -0500

    Got script.js to reset game properly

--- script.js
+++ script.js
@@ -1,80 +1,88 @@
 $(document).ready(function() {
-	var xTurn = true;
-	var xScore = 0;
-	var oScore = 0;
-	var count = 0;
+	// var xScore = 0;
+	// var oScore = 0;
+
+	var game = {
+
+		begin: function() {
+			$("td").removeClass("x");
+			$("td").removeClass("o");
+			$("td").text("");
+			var xTurn = true;
+			var count = 0;
 
 
+			$("td").on("click", function() {
+				var self = $(this);
+				if (xTurn) {
+					self.text("X");
+					self.addClass("x");
+				}
+				else {
+					$(this).text("O");
+					self.addClass("o");
+				}
+				xTurn = !xTurn;
+				count ++;
+				var game_over = false;
+				$(this).off("click");
+				if  (
+					$("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') ||
+					$("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') ||
+					$("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') ||
+					$("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') ||
+					$("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') ||
+					$("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') ||
+					$("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') ||
+					$("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x')
+					) {
+						alert("Player X has won the game!");
+						var game_over = true;
+				}
+				else if (
+					$("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') ||
+					$("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') ||
+					$("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') ||
+					$("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') ||
+					$("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') ||
+					$("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') ||
+					$("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') ||
+					$("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o')
+					) {
+						alert("Player O has won the game!");
+						var game_over = true;
+				}
+				else if (count === 9) {
+						alert("Draw!");
+						var game_over = true;
+				}
+				if (game_over) {
+					$('td').off('click');
+					setTimeout(function() {
+						$('body').click(game.begin())
+					}, 1);
+				}
+			});
+		},
+	};
 
-
-	function reset() {
-		$('body').off('click');
-		$("td").removeClass();
-		$("td").empty();
-		count = 0;
-		xTurn = true;
-	}
-
-
-
-	function render() {
-		$("#xScore").text("x Score: " + xScore);
-		$("#oScore").text("o Score: " + oScore);
-	}
-
-
-
-	$("td").on("click", function () {
-		var self = $(this);
-		if (xTurn) {
-			self.text("X");
-			self.addClass("x");
-		}
-		else {
-			$(this).text("O");
-			self.addClass("o");
-		}
-		xTurn = !xTurn;
-		$(this).off("click");
-		count ++;
-		scoreEval();
-	});
-
-
-
-	function scoreEval() {
-		if  (
-			$("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') ||
-			$("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') ||
-			$("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') ||
-			$("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') ||
-			$("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') ||
-			$("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') ||
-			$("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') ||
-			$("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x')
-			) {
-				xScore ++;
-				alert("Player X has won the game!");
-		}
-		else if (
-			$("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') ||
-			$("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') ||
-			$("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') ||
-			$("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') ||
-			$("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') ||
-			$("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') ||
-			$("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') ||
-			$("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o')
-			) {
-				oScore ++;
-				alert("Player O has won the game!");
-		}
-		else if (count === 9) {
-			alert("Draw!");
-		}
-	}
+	game.begin();
 
 });
+
+
+
+
+
+
+	// function render() {
+	// 	$("#xScore").text("x Score: " + xScore);
+	// 	$("#oScore").text("o Score: " + oScore);
+	// }
+
+
+
+
 
 
 
